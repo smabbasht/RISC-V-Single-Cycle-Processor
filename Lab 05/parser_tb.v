cfa15395c13ec865cbaec84c7a968dd7a534cd6a
abbastaqvi <79911677+abbastaqvi@users.noreply.github.com>
2022-02-25 10:44:01 +0500

    Task 2

---
+++ Lab 05/parser_tb.v
@@ -0,0 +1,20 @@
+module moduleName ();
+    reg  [31: 0] instruction,
+    wire [ 6: 0] opcode,
+    wire [11: 7] rd,
+    wire [14:12] funct3,
+    wire [19:15] rs1,
+    wire [24:20] rs2,
+    wire [31:25] funct7,
+
+    parser p1(.instruction(instruction), .opcode(opcode), .rd(rd), .funct3(funct3), .rs1(rs1), .rs2(rs2), .funct7(funct7));
+
+    initial begin
+        instruction = 32'd214748364;
+        #5
+        instruction = 32'd414748364;
+        #5
+        instruction = 32'd41474834; // dummy case
+    end
+
+endmodule
