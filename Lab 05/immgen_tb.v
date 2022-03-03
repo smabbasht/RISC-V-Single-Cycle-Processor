b1c9194b7871d38e6d4a1a87aa59fab197413071
abbastaqvi <79911677+abbastaqvi@users.noreply.github.com>
2022-03-04 02:27:48 +0500

    Lab 5 Completed

--- Lab 05/immgen_tb.v
+++ Lab 05/immgen_tb.v
@@ -2,16 +2,16 @@ module immgen_tb();
     reg [31:0] instr;
     wire [63:0] imm_value;
 
-    immediate_generator imm1(.instruction(instr), .imm_value(imm_value))
+    immediate_generator imm1(.instruction(instr), .imm_value(imm_value));
 
     initial begin
-        instr = 32'b1111 1111 1111 1111 0000 0000 0000 0000 0000 0000 0000 0000;
+        instr = 32'b11111111111100000000000000000000;
         #5
-        instr = 32'b1111 1111 1111 1111 0000 0000 0000 0000 0000 0000 0010 0000;
+        instr = 32'b11111111111100000000000000100000;
         #5
-        instr = 32'b1111 1111 1111 1111 0000 0000 0000 0000 0000 0000 0100 0000;
+        instr = 32'b11111111111100000000000001000000;
         #5
-        instr = 32'b1111 1111 1111 1111 0000 0000 0000 0000 0000 0000 0110 0000;
+        instr = 32'b11111111111100000000000001100000;
         #5
         instr = 32'd41474834; // dummy case
         $finish;
